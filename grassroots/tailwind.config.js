caca022fff6c9f91745731eb1469eba5488eaaa6
secret-star1202 <starDev1202@outlook.com>
2022-09-21 05:02:11 +0900

    variants in tailwind

--- grassroots/tailwind.config.js
+++ grassroots/tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+    fontSize: ["responsive", "hover"],
+  },
   plugins: [],
 };
