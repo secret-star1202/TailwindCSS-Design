ef4575c20a3f8d80bfda2af925458cbec299f4be
secret-star1202 <starDev1202@outlook.com>
2022-09-21 16:02:09 +0900

    toggle function

--- navbar/src/components/Navbar.vue
+++ navbar/src/components/Navbar.vue
@@ -1,21 +1,47 @@
 <template>
-  <header class="flex items-center justify-between px-4 py-3 bg-gray-900">
-    <div>
-      <img src="/img/logo-inverted.svg" alt="Workcation" class="h-8" />
+  <header class="bg-gray-900">
+    <div class="flex items-center justify-between px-4 py-3 ">
+      <div>
+        <img src="/img/logo-inverted.svg" alt="Workcation" class="h-8" />
+      </div>
+      <!-- <div class="bg-gray-700 text-white">Login</div> -->
+      <div>
+        <button
+          @click="isOpen = !isOpen"
+          type="button"
+          class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+        >
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              v-if="isOpen"
+              fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+            />
+            <path
+              v-if="!isOpen"
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-    <!-- <div class="bg-gray-700 text-white">Login</div> -->
-    <div>
-      <button
-        type="button"
-        class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4">
+      <a
+        href="#"
+        class="block px-2 py-1 text-white font-semibold hover:bg-gray-800 rounded"
+        >List your property</a
       >
-        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-          <path
-            fill-rule="evenodd"
-            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-          />
-        </svg>
-      </button>
+      <a
+        href="#"
+        class="mt-1 block px-2 py-1 text-white font-semibold hover:bg-gray-800 rounded"
+        >Trips</a
+      >
+      <a
+        href="#"
+        class="mt-1 block px-2 py-1 text-white font-semibold hover:bg-gray-800 rounded"
+        >Messages</a
+      >
     </div>
   </header>
 </template>
@@ -23,7 +49,9 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      isOpen: false,
+    };
   },
 };
 </script>
