2220b3ed563ac80660dc120775a5832641e4c4a3
secret-star1202 <starDev1202@outlook.com>
2022-09-21 15:52:28 +0900

    feat : static header

--- navbar/src/App.vue
+++ navbar/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app" class="antialiased text-gray-900">
     <div class="bg-gray-200 min-h-screen">
-      <Navbar/>
+      <Navbar />
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Navbar
+    Navbar,
   },
-}
+};
 </script>
 
 <style src="./assets/tailwind.css"></style>
